49aee5fd084ac22e1aa95342f4effe06aa5e35ec
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-25 23:32:07 +0330

    see setion 1 - video 12
    string data type

---
+++ fasle1/012/p1.js
@@ -0,0 +1,8 @@
+var name = 'meiad'
+var fullName1 = 'meiad "noushadi"'
+var fullName2 = 'meiad \"noushadi\"'
+
+console.log(name)
+console.log(fullName1)
+console.log(fullName2)
+console.log(fullName2.length)
