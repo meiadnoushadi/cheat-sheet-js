f4af10e421c7544e52c21d85feda27f6326c7eed
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-26 00:21:23 +0330

    see setion 1 - video 17
    concat string by number and boolean

--- fasle1/016/p1.js
+++ fasle1/016/p1.js
@@ -1,6 +1,6 @@
-console.log(14/3)
-console.log(14%3)
-console.log(14*3)
-console.log(14+3)
-console.log(14-3)
-console.log(14**3)
+console.log(14/3);
+console.log(14%3);
+console.log(14*3);
+console.log(14+3);
+console.log(14-3);
+console.log(14**3);
---
+++ fasle1/017/p1.js
@@ -0,0 +1,19 @@
+var a = 10;
+var b = 4;
+console.log(a+b);
+
+var c = '10';
+var d = '12';
+console.log(c+d);
+
+var name = 'meiad';
+var family = 'noushadi';
+console.log(name+' '+family);
+ 
+var flag = true
+
+console.log(name+c+a);
+console.log(a+c);
+console.log(c+a);
+console.log(flag+a);
+console.log(flag+c);
