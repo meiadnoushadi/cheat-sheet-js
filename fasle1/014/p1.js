20207f967c96b6a60f5236aef89ce3521ccb750f
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-25 23:50:44 +0330

    see setion 1 - video 14
    boolean data type

---
+++ fasle1/014/p1.js
@@ -0,0 +1,14 @@
+var a = true
+var b = false
+
+console.log(a)
+console.log(b)
+
+var c = (10>8);
+console.log(c);
+
+var d = ('milad'=='meiad');
+console.log(d);
+
+var age =22;
+console.log(age>18);
