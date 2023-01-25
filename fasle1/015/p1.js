1b519994675681f1b14a87295a757d295d945a45
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-25 23:57:06 +0330

    see setion 1 - video 15
    undfind | empty | typeof

---
+++ fasle1/015/p1.js
@@ -0,0 +1,19 @@
+var a = 250;
+console.log(typeof a);
+
+var b = '250';
+console.log(typeof b);
+
+var c = "250";
+console.log(typeof c);
+
+var d = true;
+console.log(typeof d);
+
+var e = '';
+console.log(typeof e);
+
+var f;
+console.log(f)
+console.log(typeof f);
+
