eb20acbcd3fd8af806121ef11742b995bd7c69f9
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-26 01:11:41 +0330

    see setion 1 - video 19

--- fasle1/018/p1.js
+++ fasle1/018/p1.js
@@ -1,8 +1,8 @@
 var a = 10;
-console.log(++a)
-console.log(a++)
-console.log(a)
-console.log('-----')
-console.log(--a)
-console.log(a--)
-console.log(a)
+console.log(++a);
+console.log(a++);
+console.log(a);
+console.log('-----');
+console.log(--a);
+console.log(a--);
+console.log(a);
---
+++ fasle1/019/p1.js
@@ -0,0 +1,4 @@
+var a = 10+4*2;
+console.log(a);
+var b = (10+4)*2;
+console.log(b);
