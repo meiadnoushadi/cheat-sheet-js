d11453aecc5cf4e1111c643ab61c300f9f2f1d20
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-26 19:51:01 +0330

    see setion 1 - video 22
    	comparison Operator

---
+++ fasle1/022/p1.js
@@ -0,0 +1,12 @@
+console.log(2==2);
+console.log(2=="2");
+console.log("--------------------");
+console.log(2===2);
+console.log(2==="2");
+console.log("--------------------");
+console.log(2!=2);
+console.log(2!="2");
+console.log("--------------------");
+console.log(2!==2);
+console.log(2!=="2");
+
