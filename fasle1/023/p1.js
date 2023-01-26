5000a58d3a6f4c96fc19cac43c3265057049cdbf
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-26 19:56:20 +0330

    see setion 1 - video 23
    	Logical Operator

---
+++ fasle1/023/p1.js
@@ -0,0 +1,10 @@
+console.log(true && true);
+console.log(true && false);
+console.log("--------------------");
+console.log(true || true);
+console.log(true || false);
+console.log("--------------------");
+console.log(!true);
+console.log(!false);
+
+
