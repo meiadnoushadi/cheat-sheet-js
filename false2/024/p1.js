1efeef5c5f128acb447b59df074c7e14ff0204f1
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-26 20:13:25 +0330

    see setion 2 - video 24
    	block if

---
+++ false2/024/p1.js
@@ -0,0 +1,13 @@
+if (true || true){
+    console.log('true');
+};
+var age = 20;
+if (age>18){
+    console.log('true')
+}
+var name = 'meiad'
+if (name=='ali'){
+    console.log('ture')
+}else{
+    console.log('false')
+}
