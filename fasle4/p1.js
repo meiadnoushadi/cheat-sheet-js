b4c11fca60e7d19974917eab299446e44fefee44
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-27 18:29:38 +0330

    see setion 4 - video 51 ta 54
    	nested loop

---
+++ fasle4/p1.js
@@ -0,0 +1,33 @@
+// 51,52 ***********************************
+// for(var a=1;a<=2;a++){
+//     console.log('outer '+a);
+//     for(var b=1;b<=3;b++){
+//         console.log('inner '+b);
+//     }
+// }
+
+// var str='';
+// for (var i=1;i<=2;i++){
+//     for (var j=1;j<=3;j++){
+//         // str=str+'*';
+//         str+='*';
+//     }
+//     // str=str+'\n';
+//     str+='\n';
+// }
+// console.log(str);
+
+// var str='';
+// for (var i=1;i<=5;i++){
+//     console.log(str+="*")
+// };
+
+// var str='';
+// var space='';
+// for (var i=1;i<=5;i++){
+//     for(var j=5-i;j>=1;j--){
+//         str+='*'
+//     }
+//     str+='\n'
+// }
+// console.log(str)
