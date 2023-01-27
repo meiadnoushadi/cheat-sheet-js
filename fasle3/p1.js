0b178471207b9064f7ab66175e9929478b537386
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-27 15:36:57 +0330

    see setion 3 - video 33 ta 36
    	while

---
+++ fasle3/p1.js
@@ -0,0 +1,55 @@
+// 33,34,35,36 ****************************************
+// var age =23;
+// while(age >20){
+//     console.log('hollow world')
+// };
+
+
+// var i=1;
+// while(i<=10){
+//     console.log('meiadnoushadi');
+//     i++
+// }
+
+
+// var i = 1;
+// while(i<=5){
+//     console.log('meiad');
+//     i++;
+// }
+
+
+// var num = 5;
+// while(num<=10){
+//     console.log(num);
+//     num++
+// };
+
+
+// var num = 10;
+
+// while(num <= 100 ){
+//     if(num%7==0){
+//         console.log(num);
+//     }
+//     num++;
+// }
+
+
+// var num = 100;
+// while(num <= 2000){
+//     if(num%4==0 && num %7==0 && num %3==0){
+//         console.log(num);
+//     }
+//     num++
+// }
+
+
+var sum =0,i=1;
+
+while(i<=50){
+    sum+=i;
+    i++;
+}
+console.log('sum is : '+sum)
+console.log(typeof('sum is : '+sum))
