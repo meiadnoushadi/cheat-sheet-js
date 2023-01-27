6480ea74402a406517c095db6884410d4c3e21a1
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-27 12:56:01 +0330

    see setion 2 - video  25
    	if

---
+++ false2/p1.js
@@ -0,0 +1,47 @@
+// 24 ***************************************
+// if (true || true){
+//     console.log('true');
+// };
+
+
+// var age = 20;
+// if (age>18){
+//     console.log('true')
+// }
+
+
+// var name = 'meiad'
+// if (name=='ali'){
+//     console.log('ture')
+// }else{
+//     console.log('false')
+// }
+// 25 ***************************************
+// var gender = 'male';
+// if (gender == 'male'){
+    //     console.log('تو پسری')
+// }
+// if (gender == 'famle'){
+//     console.log('تو دختری')
+// }
+
+
+// var age = 20;
+// var gender = 'male';
+// if (gender == 'male' && age >= 20){
+//     console.log('تو پسری')
+// };
+
+
+// var age = 25;
+// var gender = 'male';
+// if (age >= 20){
+//     console.log(age + '> 20')
+// };
+// if (age >= 25){
+//     console.log(age + '> 25')
+// };
+// if (age >= 30){
+//     console.log(age + '> 30')
+// };
+// 25 ***************************************
