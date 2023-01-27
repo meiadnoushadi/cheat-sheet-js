c40620ced2bfec926d4fb95b17bcf8ee181e2ef9
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-27 23:26:48 +0330

    see setion 5 - video 55 ta 65

---
+++ fasle5/p1.js
@@ -0,0 +1,83 @@
+// // 51 ta .. *********************************
+// var listName=['meiad','ali','reza','karim'];
+// const list1=['meiad','ali','reza','karim'];
+// var num=[1,2,3,4,5,6,7,8,9];
+
+// console.log(listName[0]);
+// console.log(listName[11]);
+// console.log(listName);
+// listName[1]='alireza';
+// console.log(listName);
+// console.log(typeof(listName));
+// // console.log(length(listName));
+// console.log('************')
+// var b=[];
+// console.log(b);
+// b[0]=1;
+// b[1]='meiad';
+// b[2]=true;
+// console.log(b);
+// console.log('************')
+// var c=[1,2,3];
+// c[6]=55;
+// console.log(c.length);
+// console.log(c);
+// c.length=11;
+// console.log(c);
+// console.log('************')
+// d=[1,2,3,4,'meiad'];
+// e=[1,2,3,4,'meiad'];
+// console.log(d==e);
+// console.log('************')
+// var a=3;
+// var b=4;
+// var c=8;
+// var sum=a+b+c;
+// var avg=sum/5;
+// console.log(avg);
+// console.log('************')
+// var list2=[3,4,8];
+// console.log(list2.length)
+// var s = 0;
+// for(var i=0;i<=list2.length;i++){
+//     if(list2[i]%2==0){
+//         console.log(list2[i]);
+//     }
+// }
+// console.log(s);
+
+// var array1=[1,'meiad',true];
+// console.log(array1);
+// console.log(array1.toString());
+// console.log(array1.join('-'));
+// console.log(array1.pop());
+// console.log(array1);
+// console.log(array1.push(true));
+// console.log(array1);
+// console.log(array1.shift());
+// console.log(array1);
+// console.log(array1.unshift(1));
+// console.log(array1);
+
+// var array1=[1,'meiad',true];
+// var array2=[2,'ali',false];
+
+// console.log(array1+array2);
+// console.log(array1.concat(array2));
+
+var array1=[1,2,3,4,5];
+
+// x=array1.splice(1,2,'2','3');
+// console.log(array1);
+
+// var x=array1.slice(1);
+// console.log(x,array1);
+
+var max=0
+for(var i=0;i<array1.length;i++){
+    if(array1[i]>max){
+        max=array1[i]
+    }
+}
+console.log(array1)
+console.log(max)
