aa5925a2f6332cf8b826f1e31fe246ea059f448e
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-28 22:39:59 +0330

    see setion 9 - video 91 ta 95 and ...

---
+++ fasle9/p1.js
@@ -0,0 +1,54 @@
+// var x = 10;
+// let y = 11;
+// console.log(x)
+// console.log(y)
+// if(true){
+//     var x = 100
+//     let y = 111
+//     console.log(x)
+//     console.log(y)
+// }
+// console.log(x)
+// console.log(y)
+
+
+// x : dar block main sabet nist 
+// y : dar har block meghdare moshakhas dare
+
+// const obj1={
+//     name:'meiad',
+//     family:'noushadi',
+//     propertiy:['music','coding'],
+//     function1:function(){
+//         return 'ali'
+//     },
+//     grade:{
+//         math:10,
+//         sport:20,
+//         geometry:18
+//     }
+// }
+// console.log(obj1)     
+// console.log(obj1.family)           
+// console.log(obj1.name)
+// console.log(obj1['propertiy'])
+// console.log(obj1.grade.geometry)     
+
+
+
+// let array1=[1,2,3];
+// let array2=array1;
+
+// console.log(array1);
+
+// array2[0]='meiad';
+
+// console.log(array1);
+
+const meiad={
+    1:2,
+    2:5,
+    5:13
+}
+
+// start video 96
