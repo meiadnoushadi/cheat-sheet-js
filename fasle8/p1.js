9b4b3487e0a919e4f4fb14c3d994e569a7790dd2
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-28 19:28:13 +0330

    see setion 8 - video 83 ta 90
    	function and variable

---
+++ fasle8/p1.js
@@ -0,0 +1,71 @@
+// var a=10;
+// function fun1(){
+//     let b=5;
+//     console.log(b);
+// }
+// fun1()
+
+
+// function fun2(){
+//     if(false){
+//         let c = 19;
+//     }
+//     console.log(c);
+// }
+
+// fun2();
+
+
+// console.log(firstName);
+// var firstName='meiadnoushadi';
+
+// var firstName;
+// console.log(firstName);
+// firstName='meiadnoushadi'
+
+
+// var n='meiad'
+// console.log(n)
+
+// let m='ali'
+// console.log(m)
+
+// const o='amir'
+// console.log(o)
+
+// p='reza'
+// console.log(p)
+
+// function name(){
+//     var n='meiad'; // let ,const
+// };
+// name();
+// console.log(n);
+
+// function name1(){
+//     n1='meiad'; 
+// };
+// name1();
+// console.log(n1);
+
+
+// //strict mode:
+// 'use strict'
+// n='meiad';
+// console.log(n)
+
+function fun1(){
+    'use strict';
+    var n='meiad';
+};
+
+
+
+
+
+
+
+
+
+
+
