7838fbc58d42f05b57712166172b1b5680195656
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-01-30 23:00:34 +0330

    see setion 10 - video 102 ...

---
+++ fasle10/p1.js
@@ -0,0 +1,70 @@
+// let element=document;
+// console.log(element);
+
+// let element=document.getElementById('myid');
+// console.log(element);
+// console.log(typeof(element));
+
+
+// let elements = document.getElementsByTagName('h1');
+// console.log(elements);
+// console.log(elements[1]);
+
+
+// let elements = document.getElementsByClassName('class1');
+// console.log(elements);
+// console.log(elements[1]);
+
+
+// let element=document.querySelector('p');
+// console.log(element);
+
+
+// let element=document.querySelectorAll('p');
+// console.log(element);
+
+
+// let element=document.querySelector('.container');
+// console.log(element.nodeName);
+// console.log(typeof(element.nodeName));
+
+// console.log(element.nodeType);
+// console.log(typeof(element.nodeType));
+
+// console.log(element.parentNode);
+// console.log(typeof(element.parentNode));
+
+// console.log(element.firstChild);
+// console.log(typeof(element.firstChild));
+
+// console.log(element.lastChild);
+// console.log(typeof(element.lastChild));
+
+// console.log(element.innerHTML);
+// console.log(typeof(element.innerHTML));
+
+// console.log(document.body)
+// console.log(typeof(document.body));
+
+// console.log(document.documentElement);
+// console.log(typeof(document.documentElement));
+
+let element=document.querySelector('.container');
+console.log(element.innerHTML);
+
+let element1=document.querySelector('.child')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
