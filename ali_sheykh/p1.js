c315a919f7fe7f6b5c9984e3d0f225c91a48dae2
Meiad Noushadi <meiadnoushadi@gmail.com>
2023-02-01 16:02:22 +0330

    see ali-sheykh | video 1 ta 7

---
+++ ali_sheykh/p1.js
@@ -0,0 +1,47 @@
+// alert('سلام');
+
+// log to console
+
+// console.log('hello console')
+
+// console.table({1:'m',2:'r',3:'q',4:'c'})
+
+// console.table([1,2,3,4])
+
+// console.time('a')
+// console.log('meiad')
+// console.timeEnd('a');
+
+// let name
+// console.log(name)
+
+// const name
+// console.log(name)
+
+// const nameA='lia'
+// console.log(nameA)
+
+// const person={
+//     name:'ali',
+//     age:25
+// };
+// person.name='meiad';
+// console.log(person)
+
+
+// const age=null
+// console.log(age)
+
+// const sym=Symbol()
+// console.log(sym)
+
+
+// val=444;
+// console.log(val);
+// console.log(typeof val);
+// console.log(val.length)
+
+console.log(new Date())
+console.log(typeof new Date())
+
+console.log(Number('ali'))
